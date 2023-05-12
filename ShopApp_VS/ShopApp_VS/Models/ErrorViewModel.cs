4d33ccc4bd5b6d033be88abaf0da6b60ba2d242d
Rishabh Jaga <759359@cognizant.com>
2023-05-12 13:01:40 +0530

    Initial Setup

---
+++ ShopApp_VS/ShopApp_VS/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ShopApp_VS.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
